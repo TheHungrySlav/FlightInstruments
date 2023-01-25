f523f6be0cdbc31496abffb1c6fe9c9f3ecac93b
Kyle Jenko <92830359+TheHungrySlav@users.noreply.github.com>
2023-01-25 12:35:06 -0500

    Add files via upload

--- aviation_instruments.py
+++ aviation_instruments.py
@@ -6,10 +6,10 @@ import pygame
 from pygame.locals import *
 pygame.init()
 clock = pygame.time.Clock()
-FPS = 60
+FPS = 100
 
 # Set up the drawing window
-screen = pygame.display.set_mode([700, 700])
+screen = pygame.display.set_mode([1500, 700])
 black = (0,0,0)
 white = (255,255,255)
 
@@ -19,29 +19,53 @@ running = True
 screen.fill(white)
 pygame.display.flip()
 
+# Import all pictures
 base = pygame.image.load("base.png").convert()
-tenthousandft_point = pygame.image.load("10000ft.png").convert()
-thousandft_point = pygame.image.load("1000ft.png").convert()
-hundredft_point = pygame.image.load("100ft.png").convert()
-
+airspeed_base = pygame.image.load("Airspeedgraphic.png").convert()
 # Draw the altimeter base
 screen.blit(pygame.transform.scale(base, (700, 700)), (0, 0))
+# Draw air speed indicator base
+screen.blit(pygame.transform.scale(airspeed_base, (700,700)),(750,0))
+pygame.display.update()
 
-# Draw Altimeter pointers
+# Origin Positions
 current_altitude = 0
+current_airspeed = 0
+new_airspeed = 0
+acceleration = 0
+throttle = 0
+direction = 0
 
-def altitude(current_altitude,direction):
+# Calculate airspeed
+def airspeed(current_airspeed,throttle):
+    if throttle == 1:
+        new_airspeed = current_airspeed + 1
+        if new_airspeed >=180:
+            new_airspeed = 180
+            print("Red Line!!!")
+    if throttle == -1:
+        new_airspeed = current_airspeed - 1
+        if new_airspeed < 0:
+            new_airspeed = 0
+            print("Stalling!")
+    elif throttle == 0:
+        new_airspeed = current_airspeed
+    acceleration = (new_airspeed - current_airspeed)/2    
+    return new_airspeed, acceleration
+
+# Calculate Altitude
+def altitude(current_altitude,direction,airspeed,acceleration):
     if direction == 1:
-        new_altitude = current_altitude + 10
+        new_altitude = current_altitude + (airspeed*1) + (0.5*acceleration*1) 
     if direction == -1:
-        new_altitude = current_altitude - 10
+        new_altitude = current_altitude + (airspeed*1) + (0.5*acceleration*1) 
         if current_altitude <= 0:
             new_altitude = 0
     if direction == 0:
         new_altitude = current_altitude
     return new_altitude 
 
-
+# Draw Altimeter pointers
 def altimeter(current_altitude):
 
     angle1 = -90 + (3.6*(current_altitude/10))
@@ -73,33 +97,57 @@ def altimeter(current_altitude):
     pygame.display.update()
     pygame.display.set_caption("Altimeter")
 
+# Draw Airspeed Indicator
+def airspeed_indicator(current_airspeed):
+    print(type(current_airspeed))
+    speed_point = (1110,340)
+    len = 200
+    angle = -90 + 1.8*(current_airspeed)
+    x = speed_point[0] + math.cos(math.radians(angle)) * len
+    y = speed_point[1] + math.sin(math.radians(angle)) * len
+    pygame.draw.line(screen, Color("white"), speed_point, (x,y), 1)
+    pygame.display.update()
 
-# Define starting speed
+# Main Loop
 while running:
-    current_altitude = altitude(current_altitude,0)
+    current_airspeed, acceleration = airspeed(current_airspeed,throttle)
+    airspeed_indicator(current_airspeed)
+    current_altitude = altitude(current_altitude,direction,current_airspeed,acceleration)
     altimeter(current_altitude)
     # Did the user click the window close button?
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
-    keys = pygame.key.get_pressed()         
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        throttle = 1
+        print("Airspeed increasing")
+        current_airspeed, acceleration = airspeed(current_airspeed,throttle)
+        airspeed_indicator(current_airspeed)
+    if keys[pygame.K_s]:
+        throttle = -1
+        print("Airspeed decreasing")
+        current_airspeed, acceleration = airspeed(current_airspeed,throttle)
+        airspeed_indicator(current_airspeed)       
     if keys[pygame.K_UP]:
-        #print("Altitude Increasing")
         direction = 1
-        screen.blit(pygame.transform.scale(base, (700, 700)), (0, 0))
-        current_altitude = altitude(current_altitude,direction)
+        # screen.blit(pygame.transform.scale(base, (700, 700)), (0, 0))
+        current_altitude = altitude(current_altitude,direction,current_airspeed, acceleration)
         altimeter(current_altitude)
     if keys[pygame.K_DOWN]:
-        #print("Altitude Decreasing")
         direction = -1
-        current_altitude = altitude(current_altitude,direction)
+        current_altitude = altitude(current_altitude,direction,current_airspeed, acceleration)
         altimeter(current_altitude)    
     else:
         direction = 0
-        current_altitude = altitude(current_altitude,direction)    
-        altimeter(current_altitude) 
+        throttle = 0
+        current_altitude = altitude(current_altitude,direction,current_airspeed, acceleration)  
+        altimeter(current_altitude)
+        current_airspeed = airspeed(current_airspeed,throttle)
+        airspeed_indicator(current_airspeed) 
     screen.fill((0, 0, 0))
-    screen.blit(pygame.transform.scale(base, (700, 700)), (0, 0))         
-    print(current_altitude)
+    screen.blit(pygame.transform.scale(base, (700, 700)), (0, 0)) 
+    screen.blit(pygame.transform.scale(airspeed_base, (700,700)),(750,0))        
+    #print(current_altitude)
     clock.tick(FPS)
